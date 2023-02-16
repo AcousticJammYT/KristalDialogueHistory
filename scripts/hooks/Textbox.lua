3465310478c0e00dd002de716fa3e384acec3554
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-02-16 13:31:29 -0500

    Update Textbox.lua

--- scripts/hooks/Textbox.lua
+++ scripts/hooks/Textbox.lua
@@ -13,17 +13,20 @@ function Textbox:setText(text, callback)
         if #Game:getFlag("dh_faces") > Kristal.getLibConfig("dialogue_history", "max_message") then
             table.remove(dh_faces)
         end
-        local dh_actor = Game:getFlag("dh_actor", {})
-        table.insert(dh_actor, 1, self.actor)
-        if #Game:getFlag("dh_actor") > Kristal.getLibConfig("dialogue_history", "max_message") then
-            table.remove(dh_actor)
-        end
     else
         local dh_faces = Game:getFlag("dh_faces", {})
         table.insert(dh_faces, 1, nil)
         if #Game:getFlag("dh_faces") > Kristal.getLibConfig("dialogue_history", "max_message") then
             table.remove(dh_faces)
         end
+    end
+    if self.actor then
+        local dh_actor = Game:getFlag("dh_actor", {})
+        table.insert(dh_actor, 1, self.actor)
+        if #Game:getFlag("dh_actor") > Kristal.getLibConfig("dialogue_history", "max_message") then
+            table.remove(dh_actor)
+        end
+    else
         local dh_actor = Game:getFlag("dh_actor", {})
         table.insert(dh_actor, 1, nil)
         if #Game:getFlag("dh_actor") > Kristal.getLibConfig("dialogue_history", "max_message") then
