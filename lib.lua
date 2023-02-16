ea8404d426f9f2a03d64ac2c2b3f9bf02417f55a
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-02-16 13:19:28 -0500

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -7,6 +7,7 @@ end
 function Lib:load(data, new_file, index)
     if not Game:getFlag("dh_history") then
         Game:setFlag("dh_history", { })
+        Game:setFlag("dh_faces", { })
     end
 end
 
