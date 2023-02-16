a7e5a67b9036408b13bb130073546e8c116f8518
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-02-16 13:21:17 -0500

    Update Textbox.lua

--- scripts/hooks/Textbox.lua
+++ scripts/hooks/Textbox.lua
@@ -9,4 +9,13 @@ function Textbox:setText(text, callback)
     end
 end
 
+function Textbox:setFace(face, ox, oy)
+    super:setFace(self, face, ox, oy)
+    local dh_faces = Game:getFlag("dh_faces", {})
+    table.insert(dh_faces, 1, text)
+    if #Game:getFlag("dh_faces") > Kristal.getLibConfig("dialogue_history", "max_message") then
+        table.remove(dh_faces)
+    end
+end
+
 return Textbox
