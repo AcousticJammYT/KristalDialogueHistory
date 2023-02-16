9da940a8a8f2fb21dfdb92f0daad0014dc187fab
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-02-16 12:33:04 -0500

    Update Textbox.lua

--- scripts/hooks/Textbox.lua
+++ scripts/hooks/Textbox.lua
@@ -3,7 +3,10 @@ local Textbox, super = Class(Textbox)
 function Textbox:setText(text, callback)
     super:setText(self, text, callback)
     local dh_history = Game:getFlag("dh_history", {})
-    table.insert(dh_history, text)
+    table.insert(dh_history, 1, text)
+    if #Game:getFlag("dh_history") > Game:getConfig("max_message") then
+        table.remove(dh_history)
+    end
 end
 
 return Textbox
