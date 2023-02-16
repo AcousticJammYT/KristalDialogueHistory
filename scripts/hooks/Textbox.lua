58cce4e822965eab7f5f73f0671874f93c806591
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-02-16 08:07:30 -0500

    Update Textbox.lua

--- scripts/hooks/Textbox.lua
+++ scripts/hooks/Textbox.lua
@@ -2,7 +2,7 @@ local Textbox, super = Class(Textbox)
 
 function Textbox:setText(text, callback)
     super:setText(self, text, callback)
-    local contacts = Game:getFlag("dh_history", {})
+    local dh_history = Game:getFlag("dh_history", {})
     table.insert(dh_history, text)
 end
 
